0aca7b8c46841d4355f041ee9a5db4b7bb31f895
Anthony Lu <alu34@students.cs.ubc.ca>
2025-02-10 16:40:55 -0800

    Added specification for TagComparator class

---
+++ src/main/model/TagComparator.java
@@ -0,0 +1,19 @@
+package model;
+
+// SOURCE: Practical Midterm 1 Practice Question #3
+
+// Comparator that ranks tags by comparing the number of events categorized under each one.
+public class TagComparator {
+
+    public TagComparator() {}
+
+    // REQUIRES: m1, m2 != null
+    // EFFECTS: returns -1 if t1 has a smaller number of events than t2
+    //          returns 0 if t1 has the same number of events as t2
+    //          returns +1 if t1 has a greater number of events than t2
+    public int compare(Tag t1, Tag t2) {
+        // stub
+        return 0;
+    }
+
+}
