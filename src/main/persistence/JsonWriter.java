e264a9abe2b05a136f420fe1671b2a68c33d3daa
Anthony Lu <alu34@students.cs.ubc.ca>
2025-02-19 01:43:53 -0800

    Added implementation for Json Writer class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,6 +1,8 @@
 package persistence;
 
 import model.Journal;
+import org.json.JSONObject;
+
 
 import java.io.*;
 
@@ -8,34 +10,38 @@ import java.io.*;
 
 // Represents a writer that writes JSON representation of workroom to file
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        // constructor
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of journal to file
     public void write(Journal jr) {
-        // stub
+        JSONObject json = jr.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
 }
