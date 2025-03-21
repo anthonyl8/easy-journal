9827afb28127563873033ff36a26c163770dedfd
Anthony Lu <anthonylu7678@gmail.com>
2025-03-20 22:47:13 -0700

    Added class level comment for RatingOutOfBoundsException

--- src/main/exceptions/RatingOutOfBoundsException.java
+++ src/main/exceptions/RatingOutOfBoundsException.java
@@ -1,5 +1,7 @@
 package exceptions;
 
+// Represents an exception thrown when the user inputted event rating is not 
+// between 1 and 10 inclusive
 public class RatingOutOfBoundsException extends Exception {
     
     public RatingOutOfBoundsException() {
