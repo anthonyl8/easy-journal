c8eefccf09800b142613febfb25f6340386a92d6
Anthony Lu <anthonylu7678@gmail.com>
2025-03-21 13:09:27 -0700

    Added Tab abstract class

---
+++ src/main/ui/tabs/Tab.java
@@ -0,0 +1,25 @@
+package ui.tabs;
+
+import javax.swing.*;
+
+import ui.JournalRunnerGUI;
+
+public abstract class Tab extends JPanel {
+
+    private final JournalRunnerGUI controller;
+
+    // REQUIRES: JournalRunnerGUI controller that holds this tab
+    // EFFECTS: sets the controller of this tab to the JournalRunnerGUI class instance
+    public Tab(JournalRunnerGUI controller) {
+        this.controller = controller;
+    }
+
+    // EFFECTS: returns the JournalRunnerGUI controller for this tab
+    public JournalRunnerGUI getController() {
+        return controller;
+    }
+
+    // EFFECTS: reloads the tab to its initial state
+    public abstract void reload();
+
+}
